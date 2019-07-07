0ffd6b52bb0dcd32cead4e158b9d6c0d73964167
Janett Hickel <janett.hickel@googlemail.com>
2019-07-07 21:17:49 +0200

    Updates

---
+++ precache-manifest.07c0b19be1c5a37a3f63806d52ad37a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45097b0ef47f4d55c10713bfd02acc75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8810db24447ab8129d00",
+    "url": "/static/css/2.e900f159.chunk.css"
+  },
+  {
+    "revision": "8810db24447ab8129d00",
+    "url": "/static/js/2.81dbdeae.chunk.js"
+  },
+  {
+    "revision": "ee124a2e59acf38721e4",
+    "url": "/static/js/main.eb4581e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bc54396771c64863193f7a3fe93d9cff",
+    "url": "/static/media/rpgawesome-webfont.bc543967.ttf"
+  },
+  {
+    "revision": "c243adb601cb0e965344851b1c46194f",
+    "url": "/static/media/rpgawesome-webfont.c243adb6.svg"
+  },
+  {
+    "revision": "cbfd0298f008558557418d19abfc2f42",
+    "url": "/static/media/rpgawesome-webfont.cbfd0298.eot"
+  },
+  {
+    "revision": "e31a701aa9b88125d36119acb8a10482",
+    "url": "/static/media/rpgawesome-webfont.e31a701a.woff"
+  }
+]);
